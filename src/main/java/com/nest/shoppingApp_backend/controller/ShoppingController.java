3d8aea15c2eb9f93cf978a8636509923c391c87d
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-07 22:13:26 +0530

    created addproduct

--- src/main/java/com/nest/shoppingApp_backend/controller/ShoppingController.java
+++ src/main/java/com/nest/shoppingApp_backend/controller/ShoppingController.java
@@ -1,4 +1,15 @@
 package com.nest.shoppingApp_backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ShoppingController {
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    public String AddProduct(){
+        return "successfully added product";
+    }
 }
